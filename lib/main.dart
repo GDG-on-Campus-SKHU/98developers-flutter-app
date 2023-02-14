900c8e29881585d12e2635034cc9e95f9edac979
yujinkim1 <yujin7621@me.com>
2023-02-14 22:58:29 +0900

    modi: Setting theme data

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        fontFamily: 'NotoSans',
+      ),
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
