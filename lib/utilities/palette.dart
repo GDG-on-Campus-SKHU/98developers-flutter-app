10f9788396c1984696776a4870cd1307b7ed4334
yujinkim1 <yujin7621@me.com>
2023-02-18 18:11:01 +0900

    add: Main palette

---
+++ lib/utilities/palette.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/** This class include the colors used in main color codes */
+class Palette {
+  static const white = Color(0xFFFFFFFF);
+  static const jetblack = Color(0xFF1A1A1A);
+  static const sapphire = Color(0xFF3366CC);
+  static const babyblue = Color(0xFF5588EE);
+  static const deepsliver = Color(0xFFD2D4E1);
+  static const sliver = Color(0xFFEBEBEB);
+  static const grapefruit = Color(0xFFEF5350);
+}
