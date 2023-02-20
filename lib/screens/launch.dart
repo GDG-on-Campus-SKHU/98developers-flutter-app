f3a1553c24e4ff9f0133ca2e73294c28001b5bc4
yujinkim1 <yujin7621@me.com>
2023-02-20 22:10:53 +0900

    modi(#5): Launch page indicator 적용

--- lib/screens/launch.dart
+++ lib/screens/launch.dart
@@ -10,50 +10,86 @@ class LaunchScreen extends StatefulWidget {
 }
 
 class _LaunchScreenState extends State<LaunchScreen> {
-  final _crossAxisStart = CrossAxisAlignment.start;
+  PageController _pageController = PageController();
+  int _currentIndex = 0;
+  List<Widget> _pages = [
+    Container(
+      padding: const EdgeInsets.all(9.0),
+      height: double.infinity,
+      color: Palette.white,
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        title("Reuse"),
+        subtitle("Find reusable items and make them."),
+        SizedBox(height: 10.0),
+        Placeholder()
+      ]),
+    ),
+    Container(
+      padding: const EdgeInsets.all(9.0),
+      color: Palette.white,
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        title("Recovery"),
+        subtitle("Find and use the climate community."),
+        SizedBox(height: 10.0),
+        Placeholder()
+      ]),
+    ),
+    Container(
+      padding: const EdgeInsets.all(9.0),
+      color: Palette.white,
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        title("Recycle"),
+        subtitle("Find out what you can recycle."),
+        SizedBox(height: 10.0),
+        Placeholder(),
+        ElevatedButton(onPressed: () {}, child: Text("Okay!"))
+      ]),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Palette.white,
-        body: PageView(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(9.0),
-              height: double.infinity,
-              color: Palette.white,
-              child: Column(crossAxisAlignment: _crossAxisStart, children: [
-                title("Reuse"),
-                subtitle("Find reusable items and make them."),
-                SizedBox(height: 10.0),
-                Placeholder()
-              ]),
+        body: Stack(children: [
+          PageView(
+            onPageChanged: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            controller: _pageController,
+            children: _pages,
+          ),
+          Positioned(
+            bottom: 215.0,
+            left: 0.0,
+            right: 0.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                _pages.length,
+                (index) => _buildPageIndicator(index),
+              ),
             ),
-            Container(
-              padding: const EdgeInsets.all(9.0),
-              color: Palette.white,
-              child: Column(crossAxisAlignment: _crossAxisStart, children: [
-                title("Recovery"),
-                subtitle("Find and use the climate community."),
-                SizedBox(height: 10.0),
-                Placeholder()
-              ]),
-            ),
-            Container(
-              padding: const EdgeInsets.all(9.0),
-              color: Palette.white,
-              child: Column(crossAxisAlignment: _crossAxisStart, children: [
-                title("Recycle"),
-                subtitle("Find out what you can recycle."),
-                SizedBox(height: 10.0),
-                Placeholder(),
-                ElevatedButton(onPressed: () {}, child: Text("Okay!"))
-              ]),
-            ),
-          ],
-        ),
+          ),
+        ]),
         appBar: AppBar(
           elevation: 0,
           backgroundColor: Palette.white,
         ));
+  }
+
+  Widget _buildPageIndicator(int index) {
+    bool isCurrentIndex = index == _currentIndex;
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 9.0),
+      width: isCurrentIndex ? 25.0 : 17.0,
+      height: isCurrentIndex ? 9.0 : 7.0,
+      decoration: BoxDecoration(
+        color: isCurrentIndex ? Palette.sapphire : Palette.deepsliver,
+        borderRadius: BorderRadius.circular(2),
+      ),
+    );
   }
 }
