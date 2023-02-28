65206f8f3baa3fcac8b3de50d6ce2c1577ed5b00
yujinkim1 <yujin7621@me.com>
2023-02-28 23:43:50 +0900

    modi: Dynamic color builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,128 @@
-import 'package:zikiza/screens/home.dart';
-import 'package:zikiza/utilities/palette.dart';
-import './screens/challenge.dart';
-import './screens/profile.dart';
 import 'package:flutter/material.dart';
+import 'package:dynamic_color/dynamic_color.dart';
+import 'package:zikiza/screens/home.dart';
+import 'package:zikiza/screens/challenge.dart';
+//import 'package:zikiza/screens/explore.dart';
+import 'package:zikiza/screens/profile.dart';
+import 'package:zikiza/utilities/dynamic_theme.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.light,
-        fontFamily: 'NotoSans',
-      ),
-      home: BottonNavigation(),
-    );
+  Widget build(BuildContext _) {
+    return DynamicColorBuilder(
+        builder: (ColorScheme? lightColorScheme, ColorScheme? darkColorScheme) {
+      return MaterialApp(
+        theme: DynamicTheme.lightTheme(lightColorScheme),
+        darkTheme: DynamicTheme.darkTheme(darkColorScheme),
+        //Default theme mode setting
+        themeMode: ThemeMode.system,
+        home: BottomNavigationBar(),
+      );
+    });
   }
 }
 
-class BottonNavigation extends StatefulWidget {
-  const BottonNavigation({super.key});
+class BottomNavigationBar extends StatefulWidget {
+  const BottomNavigationBar({super.key});
 
   @override
-  State<BottonNavigation> createState() => _BottonNavigationState();
+  State<BottomNavigationBar> createState() => _BottomNavigationBarState();
 }
 
-class _BottonNavigationState extends State<BottonNavigation> {
+class _BottomNavigationBarState extends State<BottomNavigationBar> {
   int _selectedIndex = 0;
 
   @override
   void initState() {
-    //해당 클래스가 호출되었을떄
     super.initState();
   }
 
   @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext _) {
     return Scaffold(
       body: const [
         HomeScreen(),
         ChallengeScreen(),
         HomeScreen(),
-        ProfileScreen()
+        ProfileScreen(),
       ][_selectedIndex],
       bottomNavigationBar: NavigationBar(
-        backgroundColor: Palette.white,
-        selectedIndex: _selectedIndex,
         onDestinationSelected: (int index) {
           setState(() {
             _selectedIndex = index;
           });
         },
-        destinations: const <NavigationDestination>[
+        destinations: <NavigationDestination>[
           NavigationDestination(
             selectedIcon: Icon(
               Icons.home,
-              size: 35,
-              color: Palette.babyblue,
+              size: 25,
+              color: Theme.of(_).colorScheme.primary,
             ),
             icon: Icon(
               Icons.home,
-              size: 35,
+              size: 25,
+              color: Theme.of(_).colorScheme.secondary,
             ),
             label: 'Home',
           ),
           NavigationDestination(
             selectedIcon: Icon(
               Icons.local_fire_department_rounded,
-              size: 35,
-              color: Palette.babyblue,
+              size: 25,
+              color: Theme.of(_).colorScheme.primary,
             ),
             icon: Icon(
               Icons.local_fire_department_rounded,
-              size: 35,
+              size: 25,
+              color: Theme.of(_).colorScheme.secondary,
             ),
             label: 'Challenges',
           ),
           NavigationDestination(
             selectedIcon: Icon(
-              Icons.near_me_rounded,
-              size: 35,
-              color: Palette.babyblue,
+              Icons.explore_rounded,
+              size: 25,
+              color: Theme.of(_).colorScheme.primary,
             ),
             icon: Icon(
-              Icons.near_me_rounded,
-              size: 35,
+              Icons.explore_rounded,
+              size: 25,
+              color: Theme.of(_).colorScheme.secondary,
             ),
             label: 'Explore',
           ),
           NavigationDestination(
             selectedIcon: Icon(
               Icons.person,
-              size: 35,
-              color: Palette.babyblue,
+              size: 25,
+              color: Theme.of(_).colorScheme.primary,
             ),
             icon: Icon(
               Icons.person,
-              size: 35,
+              size: 25,
+              color: Theme.of(_).colorScheme.secondary,
             ),
             label: 'Profile',
           ),
         ],
+        elevation: 2.0,
+        backgroundColor: Theme.of(_).colorScheme.background,
+        shadowColor: Theme.of(_).colorScheme.shadow,
+        surfaceTintColor: Theme.of(_).colorScheme.surfaceTint,
+        selectedIndex: _selectedIndex,
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
