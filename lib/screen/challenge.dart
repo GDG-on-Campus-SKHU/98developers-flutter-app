409b076c4f2a9c004dd88f58c5d851b9caed5d57
devpark435 <devpark435@gmail.com>
2023-02-28 15:17:11 +0900

    modi(#11) : 위젯 스타일 변경

--- lib/screen/challenge.dart
+++ lib/screen/challenge.dart
@@ -10,9 +10,7 @@ class ChallengeScreen extends StatefulWidget {
 
 class _ChallengeScreenState extends State<ChallengeScreen> {
   List<String> nowTitle = [
-    'Do not use disposable items',
-    'Walk on the wanted position',
-    'Visiting recycling Club'
+    'Eating home-cooked food',
   ];
   List<String> exportTitle = [
     'Visiting recycling Club',
@@ -22,7 +20,21 @@ class _ChallengeScreenState extends State<ChallengeScreen> {
     final _width = MediaQuery.of(context).size.width;
     final _height = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: Palette.white,
+      appBar: AppBar(
+        toolbarHeight: 80,
+        backgroundColor: Color(0xFFEDEEFA),
+        title: Container(
+          width: _width,
+          child: Text(
+            'Challenge',
+            style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w700,
+                color: Palette.jetblack),
+          ),
+        ),
+      ),
+      backgroundColor: Color(0xFFFEFBFF),
       body: SafeArea(
           child: Column(
         children: [
@@ -31,110 +43,136 @@ class _ChallengeScreenState extends State<ChallengeScreen> {
           ),
           Container(
             width: _width,
+            height: 60,
             child: Padding(
               padding: const EdgeInsets.only(left: 20),
               child: Text(
                 'NOW',
                 style: TextStyle(
-                    fontSize: 25,
-                    color: Palette.jetblack,
+                    fontSize: 32,
+                    color: Palette.babyblue,
                     fontWeight: FontWeight.w900),
               ),
             ),
           ),
-          SizedBox(
-            height: 36,
-          ),
           Container(
             //now listview
             width: _width * 0.9,
-            height: _height * 0.3,
+            height: _height * 0.15,
             child: ListView.builder(
                 itemCount: nowTitle.length,
                 itemBuilder: (context, index) {
-                  return Container(
-                    width: _width * 0.9,
-                    height: 80,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
+                  return GestureDetector(
+                    child: Container(
+                      width: _width * 0.9,
+                      height: 100,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+                          color: Color.fromARGB(255, 217, 226, 255)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Row(
                           children: [
-                            Container(
-                              width: _width * 0.78,
-                              alignment: Alignment.topLeft,
-                              child: Container(
-                                alignment: Alignment.center,
-                                width: 50,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(25),
-                                    color: Palette.grapefruit),
-                                child: Text(
-                                  '7days',
-                                  style: TextStyle(
-                                    fontSize: 11,
-                                    color: Palette.white,
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  width: _width * 0.7,
+                                  child: Text(
+                                    '${nowTitle[index]}',
+                                    style: TextStyle(
+                                        fontSize: 19,
+                                        color: Palette.jetblack,
+                                        fontWeight: FontWeight.w700),
                                   ),
                                 ),
-                              ),
+                                Container(
+                                    width: _width * 0.7,
+                                    child: Row(
+                                      children: [
+                                        Container(
+                                          alignment: Alignment.center,
+                                          height: 28,
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(25),
+                                              color: Palette.sapphire),
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 10.0, right: 10),
+                                            child: Row(
+                                              children: [
+                                                Icon(
+                                                  Icons.person,
+                                                  size: 16,
+                                                  color: Palette.white,
+                                                ),
+                                                Text(
+                                                  ' 379',
+                                                  style: TextStyle(
+                                                      color: Palette.white,
+                                                      fontSize: 13,
+                                                      fontWeight:
+                                                          FontWeight.w500),
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Container(
+                                          alignment: Alignment.center,
+                                          height: 28,
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(25),
+                                              color: Palette.sapphire),
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 10.0, right: 10),
+                                            child: Text('21days left',
+                                                style: TextStyle(
+                                                    color: Palette.white,
+                                                    fontSize: 13,
+                                                    fontWeight:
+                                                        FontWeight.w500)),
+                                          ),
+                                        )
+                                      ],
+                                    )),
+                              ],
                             ),
                             Container(
-                              width: _width * 0.78,
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                nowTitle[index],
-                                style: TextStyle(
-                                    fontSize: 19,
-                                    fontWeight: FontWeight.w700,
-                                    color: Palette.babyblue),
+                              height: 100,
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.local_fire_department_rounded,
+                                  color: Palette.grapefruit,
+                                  size: 33,
+                                ),
+                                onPressed: () {},
                               ),
-                            ),
-                            Container(
-                              width: _width * 0.78,
-                              alignment: Alignment.centerLeft,
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    Icons.person,
-                                    color: Palette.grey,
-                                    size: 13,
-                                  ),
-                                  Text(
-                                    '379 joined',
-                                    style: TextStyle(
-                                        fontSize: 13,
-                                        fontWeight: FontWeight.w500,
-                                        color: Palette.grey),
-                                  ),
-                                ],
-                              ),
-                            ),
+                            )
                           ],
                         ),
-                        Container(
-                            child: IconButton(
-                          icon: Icon(
-                            Icons.arrow_forward,
-                            size: 16,
-                            color: Palette.darkgrey,
-                          ),
-                          onPressed: () {},
-                        ))
-                      ],
+                      ),
                     ),
+                    onTap: () {},
                   );
                 }),
           ),
           Container(
             width: _width,
-            height: _height * 0.1,
+            height: 60,
             child: Padding(
               padding: const EdgeInsets.only(left: 20),
               child: Text(
-                'EXPORT',
+                'Available',
                 style: TextStyle(
-                    fontSize: 25,
-                    color: Palette.jetblack,
+                    fontSize: 32,
+                    color: Palette.babyblue,
                     fontWeight: FontWeight.w900),
               ),
             ),
@@ -148,72 +186,100 @@ class _ChallengeScreenState extends State<ChallengeScreen> {
                 itemBuilder: (context, index) {
                   return Container(
                     width: _width * 0.9,
-                    height: 80,
-                    child: Row(
-                      children: [
-                        Column(
-                          children: [
-                            Container(
-                              width: _width * 0.78,
-                              alignment: Alignment.topLeft,
-                              child: Container(
-                                alignment: Alignment.center,
-                                width: 50,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(25),
-                                    color: Palette.darkgrey),
+                    height: 100,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        color: Color.fromARGB(255, 217, 226, 255)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Row(
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                width: _width * 0.7,
                                 child: Text(
-                                  '7days',
+                                  '${exportTitle[index]}',
                                   style: TextStyle(
-                                    fontSize: 11,
-                                    color: Palette.white,
-                                  ),
+                                      fontSize: 19,
+                                      color: Palette.jetblack,
+                                      fontWeight: FontWeight.w700),
                                 ),
                               ),
-                            ),
-                            Container(
-                              width: _width * 0.78,
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                exportTitle[index],
-                                style: TextStyle(
-                                    fontSize: 19,
-                                    fontWeight: FontWeight.w700,
-                                    color: Palette.babyblue),
-                              ),
-                            ),
-                            Container(
-                              width: _width * 0.78,
-                              alignment: Alignment.centerLeft,
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    Icons.person,
-                                    color: Palette.grey,
-                                    size: 13,
-                                  ),
-                                  Text(
-                                    '379 added',
-                                    style: TextStyle(
-                                        fontSize: 13,
-                                        fontWeight: FontWeight.w500,
-                                        color: Palette.grey),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                        Container(
-                            child: IconButton(
-                          icon: Icon(
-                            Icons.arrow_forward,
-                            size: 16,
-                            color: Palette.darkgrey,
+                              Container(
+                                  width: _width * 0.7,
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        alignment: Alignment.center,
+                                        height: 28,
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(25),
+                                            color: Palette.sapphire),
+                                        child: Padding(
+                                          padding: const EdgeInsets.only(
+                                              left: 10.0, right: 10),
+                                          child: Row(
+                                            children: [
+                                              Icon(
+                                                Icons.person,
+                                                size: 16,
+                                                color: Palette.white,
+                                              ),
+                                              Text(
+                                                ' 18',
+                                                style: TextStyle(
+                                                    color: Palette.white,
+                                                    fontSize: 13,
+                                                    fontWeight:
+                                                        FontWeight.w500),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Container(
+                                        alignment: Alignment.center,
+                                        height: 28,
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(25),
+                                            color: Palette.sapphire),
+                                        child: Padding(
+                                          padding: const EdgeInsets.only(
+                                              left: 10.0, right: 10),
+                                          child: Text('21days left',
+                                              style: TextStyle(
+                                                  color: Palette.white,
+                                                  fontSize: 13,
+                                                  fontWeight: FontWeight.w500)),
+                                        ),
+                                      )
+                                    ],
+                                  )),
+                            ],
                           ),
-                          onPressed: () {},
-                        ))
-                      ],
+                          Container(
+                            height: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Palette.sapphire),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.arrow_forward,
+                                color: Palette.white,
+                                size: 33,
+                              ),
+                              onPressed: () {},
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   );
                 }),
