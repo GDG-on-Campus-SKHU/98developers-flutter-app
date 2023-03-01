d122b2ffce4cced57c2a8aa990a48a621dfd2e5e
yujinkim1 <yujin7621@me.com>
2023-03-01 17:10:24 +0900

    refactor(#25): Segmented widget and state with cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zikiza/screens/home.dart';
 import 'package:zikiza/screens/challenge.dart';
 //import 'package:zikiza/screens/explore.dart';
 import 'package:zikiza/screens/profile.dart';
 import 'package:zikiza/utilities/dynamic_theme.dart';
+import 'package:zikiza/cubits/navigation_bar_cubit.dart';
 
 void main() {
   runApp(const MainApp());
@@ -20,109 +22,99 @@ class MainApp extends StatelessWidget {
       return MaterialApp(
         theme: DynamicTheme.lightTheme(lightColorScheme),
         darkTheme: DynamicTheme.darkTheme(darkColorScheme),
-        //Default theme mode setting
+        //디바이스 시스템 설정에 따라 theme 모드 전환
         themeMode: ThemeMode.system,
-        home: BottomNavigationBar(),
+        home: BlocProvider(
+          create: (_) => NavigationBarCubit(),
+          child: NavigationBarWidget(),
+        ),
       );
     });
   }
 }
 
-class BottomNavigationBar extends StatefulWidget {
-  const BottomNavigationBar({super.key});
-
-  @override
-  State<BottomNavigationBar> createState() => _BottomNavigationBarState();
-}
-
-class _BottomNavigationBarState extends State<BottomNavigationBar> {
-  int _selectedIndex = 0;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
+class NavigationBarWidget extends StatelessWidget {
   @override
   Widget build(BuildContext _) {
-    return Scaffold(
-      body: const [
-        HomeScreen(),
-        ChallengeScreen(),
-        HomeScreen(),
-        ProfileScreen(),
-      ][_selectedIndex],
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        destinations: <NavigationDestination>[
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.home,
-              size: 25,
-              color: Theme.of(_).colorScheme.primary,
-            ),
-            icon: Icon(
-              Icons.home,
-              size: 25,
-              color: Theme.of(_).colorScheme.secondary,
-            ),
-            label: 'Home',
+    return BlocBuilder<NavigationBarCubit, int>(
+      builder: (_, state) {
+        return Scaffold(
+          body: const [
+            HomeScreen(),
+            ChallengeScreen(),
+            HomeScreen(),
+            ProfileScreen(),
+          ][state],
+          bottomNavigationBar: BlocBuilder<NavigationBarCubit, int>(
+            builder: (_, state) {
+              return NavigationBar(
+                selectedIndex: state,
+                onDestinationSelected: (index) => _
+                    .read<NavigationBarCubit>()
+                    .updateDestinationSelectedIndex(index),
+                destinations: <NavigationDestination>[
+                  NavigationDestination(
+                    selectedIcon: Icon(
+                      Icons.home,
+                      size: 25,
+                      color: Theme.of(_).colorScheme.primary,
+                    ),
+                    icon: Icon(
+                      Icons.home,
+                      size: 25,
+                      color: Theme.of(_).colorScheme.secondary,
+                    ),
+                    label: 'Home',
+                  ),
+                  NavigationDestination(
+                    selectedIcon: Icon(
+                      Icons.workspace_premium_rounded,
+                      size: 25,
+                      color: Theme.of(_).colorScheme.primary,
+                    ),
+                    icon: Icon(
+                      Icons.workspace_premium_rounded,
+                      size: 25,
+                      color: Theme.of(_).colorScheme.secondary,
+                    ),
+                    label: 'Challenges',
+                  ),
+                  NavigationDestination(
+                    selectedIcon: Icon(
+                      Icons.explore_rounded,
+                      size: 25,
+                      color: Theme.of(_).colorScheme.primary,
+                    ),
+                    icon: Icon(
+                      Icons.explore_rounded,
+                      size: 25,
+                      color: Theme.of(_).colorScheme.secondary,
+                    ),
+                    label: 'Explore',
+                  ),
+                  NavigationDestination(
+                    selectedIcon: Icon(
+                      Icons.person,
+                      size: 25,
+                      color: Theme.of(_).colorScheme.primary,
+                    ),
+                    icon: Icon(
+                      Icons.person,
+                      size: 25,
+                      color: Theme.of(_).colorScheme.secondary,
+                    ),
+                    label: 'Profile',
+                  ),
+                ],
+                elevation: 2.0,
+                backgroundColor: Theme.of(_).colorScheme.background,
+                shadowColor: Theme.of(_).colorScheme.shadow,
+                surfaceTintColor: Theme.of(_).colorScheme.surfaceTint,
+              );
+            },
           ),
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.local_fire_department_rounded,
-              size: 25,
-              color: Theme.of(_).colorScheme.primary,
-            ),
-            icon: Icon(
-              Icons.local_fire_department_rounded,
-              size: 25,
-              color: Theme.of(_).colorScheme.secondary,
-            ),
-            label: 'Challenges',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.explore_rounded,
-              size: 25,
-              color: Theme.of(_).colorScheme.primary,
-            ),
-            icon: Icon(
-              Icons.explore_rounded,
-              size: 25,
-              color: Theme.of(_).colorScheme.secondary,
-            ),
-            label: 'Explore',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.person,
-              size: 25,
-              color: Theme.of(_).colorScheme.primary,
-            ),
-            icon: Icon(
-              Icons.person,
-              size: 25,
-              color: Theme.of(_).colorScheme.secondary,
-            ),
-            label: 'Profile',
-          ),
-        ],
-        elevation: 2.0,
-        backgroundColor: Theme.of(_).colorScheme.background,
-        shadowColor: Theme.of(_).colorScheme.shadow,
-        surfaceTintColor: Theme.of(_).colorScheme.surfaceTint,
-        selectedIndex: _selectedIndex,
-      ),
+        );
+      },
     );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
   }
 }
