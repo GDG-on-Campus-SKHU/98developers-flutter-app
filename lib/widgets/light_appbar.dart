b7adc9e90ebac57689372821771196c034f46b5a
yujinkim1 <yujin7621@me.com>
2023-03-04 17:35:05 +0900

    modi: Adjust preferredsize

--- lib/widgets/light_appbar.dart
+++ lib/widgets/light_appbar.dart
@@ -1,26 +1,32 @@
-import 'package:zikiza/utilities/palette.dart';
 import 'package:flutter/material.dart';
 
 class LightAppBar extends StatelessWidget implements PreferredSizeWidget {
-  LightAppBar(
-      {Key? key,
-      required this.actions,
-      required Text title,
-      required Color color});
+  LightAppBar({
+    Key? key,
+    required this.actions,
+    required this.title,
+    required this.color,
+    required this.backgroundColor,
+  });
 
   final List<Widget>? actions;
+  final Widget? title;
+  final Color color;
+  final Color backgroundColor;
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       actions: actions,
+      title: title,
       scrolledUnderElevation: 0,
       elevation: 0,
-      backgroundColor: Palette.white,
+      toolbarHeight: 65.0,
+      backgroundColor: backgroundColor,
       automaticallyImplyLeading: false,
     );
   }
 
   @override
-  Size get preferredSize => Size(0.0, 55.0);
+  Size get preferredSize => Size(0.0, 65.0);
 }
