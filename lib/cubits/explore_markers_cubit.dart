d438ee45b18231ce099eb90072a56edee775d19a
yujinkim1 <yujin7621@me.com>
2023-03-12 23:52:04 +0900

    delete: Explore custom marker state class

--- lib/cubits/explore_markers_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'explore_markers_state.dart';
-
-class ExploreMarkersCubit extends Cubit<ExploreMarkersState> {
-  ExploreMarkersCubit() : super(ExploreMarkersInitial());
-}
