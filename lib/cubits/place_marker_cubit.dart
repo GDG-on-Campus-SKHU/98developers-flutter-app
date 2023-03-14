20c3fb9fc3a6afd7e6926051a6cd0fe86eca5cc5
yujinkim1 <yujin7621@me.com>
2023-03-14 15:31:29 +0900

    delete: Place marker cubit and state class

--- lib/cubits/place_marker_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'place_marker_state.dart';
-
-class PlaceMarkerCubit extends Cubit<PlaceMarkerState> {
-  PlaceMarkerCubit() : super(PlaceMarkerInitial());
-}
