c1aaff90ddf01e8a3f70c70c086c5802ef2309da
yujinkim1 <yujin7621@me.com>
2023-03-14 13:53:33 +0900

    modi: Remove Nested list class

--- lib/models/explore_bundle.dart
+++ lib/models/explore_bundle.dart
@@ -1,20 +1,3 @@
-import 'dart:convert';
-
-class ExploreBundle {
-  final List<ExploreData> exploreBundle;
-
-  ExploreBundle({
-    required this.exploreBundle,
-  });
-
-  factory ExploreBundle.fromJson(List<dynamic> parsedJson) {
-    List<ExploreData> exploreBundle = [];
-    exploreBundle = parsedJson.map((e) => ExploreData.fromRawJson(e)).toList();
-
-    return new ExploreBundle(exploreBundle: exploreBundle);
-  }
-}
-
 class ExploreData {
   final int? id;
   final String? name;
@@ -26,30 +9,25 @@ class ExploreData {
   final double? longitude;
 
   ExploreData({
-    this.id,
+    required this.id,
     this.name,
     this.tag,
     this.address,
     this.tele,
     this.image,
-    this.latitude,
-    this.longitude,
+    required this.latitude,
+    required this.longitude,
   });
 
-  factory ExploreData.fromRawJson(String str) =>
-      ExploreData.fromJson(json.decode(str));
-
-  String toRawJson() => json.encode(toJson());
-
   factory ExploreData.fromJson(Map<String, dynamic> json) => ExploreData(
-        id: json["id"],
-        name: json["name"],
-        tag: json["tag"] ?? "",
-        address: json["address"],
-        tele: json["tele"] ?? "",
-        image: json["image"],
-        latitude: json["latitude"].toDouble(),
-        longitude: json["longitude"].toDouble(),
+        id: json["id"] ?? 0,
+        name: json["name"] ?? "-",
+        tag: json["tag"] ?? "-",
+        address: json["address"] ?? "-",
+        tele: json["tele"] ?? "-",
+        image: json["image"] ?? "-",
+        latitude: json["latitude"],
+        longitude: json["longitude"],
       );
 
   Map<String, dynamic> toJson() => {
