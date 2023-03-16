edcd25b9b545e0f9a4bd36a264f12b9357d3e658
devpark435 <devpark435@gmail.com>
2023-03-16 17:55:10 +0900

    feat(#43) : add take challenge photo

--- lib/models/getUserData.dart
+++ lib/models/getUserData.dart
@@ -1,20 +1,119 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
-Future getUserData() async {
+Future<getUserModel> getUserData() async {
   /**userData 조회 */
   String url = "https://zikiza.duckdns.org/users";
   final FlutterSecureStorage _secureStorage = FlutterSecureStorage();
   String? token = await _secureStorage.read(key: 'id_token');
+  getUserModel? userModel;
 
   var response = await http.get(
     Uri.parse(url),
     headers: {"Authorization": "Bearer " + token.toString()},
   );
 
-  if (response.statusCode == 200) {
-    print("챌린지 등록 성공 ${response.body}");
-  } else {
-    print(response.body);
+  final response_data = utf8.decode(response.bodyBytes);
+  log("getUserData(): [200]Http get Challenge data successfully.\n${response_data}");
+  var data = json.decode(utf8.decode(response.bodyBytes));
+  userModel = getUserModel(
+    uid: data['uid'],
+    email: data['email'],
+    name: data['name'],
+    challenges: data['challenges'] != null
+        ? [Challenges.fromJson(data['challenges'][0])]
+        : null,
+  );
+  return userModel;
+}
+
+class getUserModel {
+  String? uid;
+  String? email;
+  String? name;
+  String? avatar;
+  List<Challenges>? challenges;
+
+  getUserModel({this.uid, this.email, this.name, this.avatar, this.challenges});
+
+  getUserModel.fromJson(Map<String, dynamic> json) {
+    uid = json['uid'];
+    email = json['email'];
+    name = json['name'];
+    avatar = json['avatar'];
+    if (json['challenges'] != null) {
+      challenges = <Challenges>[];
+      json['challenges'].forEach((v) {
+        challenges!.add(new Challenges.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['uid'] = this.uid;
+    data['email'] = this.email;
+    data['name'] = this.name;
+    data['avatar'] = this.avatar;
+    if (this.challenges != null) {
+      data['challenges'] = this.challenges!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Challenges {
+  int? id;
+  String? topic;
+  String? howProof;
+  String? expectedResults;
+  String? pleaseNote;
+  String? correctProof;
+  String? periodStartDate;
+  String? periodEndDate;
+  String? expiredDay;
+  int? members;
+
+  Challenges(
+      {this.id,
+      this.topic,
+      this.howProof,
+      this.expectedResults,
+      this.pleaseNote,
+      this.correctProof,
+      this.periodStartDate,
+      this.periodEndDate,
+      this.expiredDay,
+      this.members});
+
+  Challenges.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    topic = json['topic'];
+    howProof = json['howProof'];
+    expectedResults = json['expectedResults'];
+    pleaseNote = json['pleaseNote'];
+    correctProof = json['correctProof'];
+    periodStartDate = json['periodStartDate'];
+    periodEndDate = json['periodEndDate'];
+    expiredDay = json['expiredDay'];
+    members = json['members'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['topic'] = this.topic;
+    data['howProof'] = this.howProof;
+    data['expectedResults'] = this.expectedResults;
+    data['pleaseNote'] = this.pleaseNote;
+    data['correctProof'] = this.correctProof;
+    data['periodStartDate'] = this.periodStartDate;
+    data['periodEndDate'] = this.periodEndDate;
+    data['expiredDay'] = this.expiredDay;
+    data['members'] = this.members;
+    return data;
   }
 }
--- lib/screens/submission.dart
+++ lib/screens/submission.dart
@@ -4,9 +4,13 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:image_picker/image_picker.dart';
+import 'package:zikiza/models/post_submission.dart';
+
+import '../models/getChallenge.dart';
 
 class submissionScreen extends StatefulWidget {
-  const submissionScreen({super.key});
+  const submissionScreen({super.key, required this.id});
+  final id;
 
   @override
   State<submissionScreen> createState() => _submissionScreenState();
@@ -48,8 +52,11 @@ class _submissionScreenState extends State<submissionScreen> {
   }
 
   _getCameraImage() async {
-    final pickedFile =
-        await ImagePicker().pickImage(source: ImageSource.camera);
+    final pickedFile = await ImagePicker().pickImage(
+        source: ImageSource.camera,
+        maxWidth: 100,
+        maxHeight: 100,
+        imageQuality: 50);
     if (pickedFile != null) {
       setState(() {
         _pickedFile = pickedFile;
@@ -78,94 +85,140 @@ class _submissionScreenState extends State<submissionScreen> {
         height: _height * 0.7,
         color: dynamicColor.background,
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                width: _width,
-                padding: EdgeInsets.only(left: 20, top: 20),
-                child: Text(
-                  '제목',
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w700,
-                      color: dynamicColor.onBackground),
-                ),
-              ),
-              Container(
-                width: _width,
-                padding: EdgeInsets.only(left: 20, top: 20),
-                child: Text(
-                  '목표',
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w700,
-                      color: dynamicColor.onBackground),
-                ),
-              ),
-              Container(
-                width: _width,
-                padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
-                child: Text(
-                  '기한',
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w700,
-                      color: dynamicColor.onBackground),
-                ),
-              ),
-              Container(
-                width: _width * 0.9,
-                height: 300,
-                decoration: BoxDecoration(
-                    border: Border.all(
-                        width: 1, color: dynamicColor.secondaryContainer)),
-                child: _pickedFile == null
-                    ? Container(
-                        child: GestureDetector(
-                          onTap: () {
-                            _showBottomSheet(
-                                dynamicColor.background,
-                                dynamicColor.secondaryContainer,
-                                dynamicColor.onSecondaryContainer);
-                          },
-                          child: Center(child: Text('이미지 제출 하셈')),
-                        ),
-                      )
-                    : Center(
-                        child: Container(
-                          width: _width * 0.8,
-                          height: 250,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: FileImage(File(_pickedFile!.path)),
-                                fit: BoxFit.cover),
-                          ),
-                        ),
-                      ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              _pickedFile == null
-                  ? Container(
-                      width: _width * 0.5,
-                      height: 100,
-                      child: Text('아직 제출 못함 ㅅㄱ'),
-                    )
-                  : Container(
-                      width: _width * 0.5,
-                      height: 50,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(25),
-                          color: dynamicColor.secondaryContainer),
+          child: FutureBuilder(
+            future: getChallenges(widget.id),
+            builder: (BuildContext context, snapshot) {
+              if (snapshot.hasData) {
+                var topic = snapshot.data!.topic;
+                var exResult = snapshot.data!.expectedResults;
+
+                return Column(
+                  children: [
+                    Container(
+                      width: _width,
+                      padding: EdgeInsets.only(left: 20, top: 20),
                       child: Text(
-                        '제출하기',
-                        style:
-                            TextStyle(color: dynamicColor.onSecondaryContainer),
+                        '제목',
+                        style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                            color: dynamicColor.onBackground),
                       ),
-                    )
-            ],
+                    ),
+                    Container(
+                      width: _width,
+                      padding: EdgeInsets.only(
+                        left: 20,
+                      ),
+                      child: Text(
+                        '$topic',
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                            color: dynamicColor.onBackground),
+                      ),
+                    ),
+                    Container(
+                      width: _width,
+                      padding: EdgeInsets.only(left: 20, top: 20),
+                      child: Text(
+                        '목표',
+                        style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                            color: dynamicColor.onBackground),
+                      ),
+                    ),
+                    Container(
+                      width: _width,
+                      padding: EdgeInsets.only(
+                        left: 20,
+                      ),
+                      child: Text(
+                        '$exResult',
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                            color: dynamicColor.onBackground),
+                      ),
+                    ),
+                    Container(
+                      width: _width,
+                      padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                      child: Text(
+                        '기한',
+                        style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                            color: dynamicColor.onBackground),
+                      ),
+                    ),
+                    Container(
+                      width: _width * 0.9,
+                      height: 300,
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 1,
+                              color: dynamicColor.secondaryContainer)),
+                      child: _pickedFile == null
+                          ? Container(
+                              child: GestureDetector(
+                                onTap: () {
+                                  _showBottomSheet(
+                                      dynamicColor.background,
+                                      dynamicColor.secondaryContainer,
+                                      dynamicColor.onSecondaryContainer);
+                                },
+                                child: Center(child: Text('이미지 제출 하셈')),
+                              ),
+                            )
+                          : Center(
+                              child: Container(
+                                width: 200,
+                                height: 200,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: FileImage(File(_pickedFile!.path)),
+                                      fit: BoxFit.cover),
+                                ),
+                              ),
+                            ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    _pickedFile == null
+                        ? Container(
+                            width: _width * 0.5,
+                            height: 100,
+                            child: Text('아직 제출 못함 ㅅㄱ'),
+                          )
+                        : GestureDetector(
+                            child: Container(
+                              width: _width * 0.5,
+                              height: 50,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(25),
+                                  color: dynamicColor.secondaryContainer),
+                              child: Text(
+                                '제출하기',
+                                style: TextStyle(
+                                    color: dynamicColor.onSecondaryContainer),
+                              ),
+                            ),
+                            onTap: () {
+                              postSubmission(widget.id, _pickedFile!.path);
+                            },
+                          )
+                  ],
+                );
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
           ),
         ),
       )),
