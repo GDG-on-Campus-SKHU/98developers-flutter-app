4723c3b1abd77389abd37b53ffd8d04e019ffb53
Yujin Kim <yujinkim@Yujin-Kim-MacBook-Pro.local>
2023-03-19 22:54:21 +0900

    modi: Exclude unused colors in palette

--- lib/utilities/palette.dart
+++ lib/utilities/palette.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
-/** This class include the colors used in main color codes */
+/**
+ * 위젯의 색상을 커스터마이징 하기 위한 컬러 클래스
+ * sapphire를 제외하고 다른 색상은 사용하지 않으므로 주석처리
+*/
 class Palette {
-  static const white = Color(0xFFFFFFFF);
-  static const jetblack = Color(0xFF1A1A1A);
-  static const darkgrey = Color(0xFF858585);
-  static const grey = Color(0xFFECDCDCD);
+  // static const white = Color(0xFFFFFFFF);
+  // static const jetblack = Color(0xFF1A1A1A);
+  // static const darkgrey = Color(0xFF858585);
+  // static const grey = Color(0xFFECDCDCD);
   static const sapphire = Color(0xFF3366CC);
-  static const babyblue = Color(0xFF5588EE);
-  static const deepsliver = Color(0xFFD2D4E1);
-  static const sliver = Color(0xFFEBEBEB);
-  static const grapefruit = Color(0xFFEF5350);
+  // static const babyblue = Color(0xFF5588EE);
+  // static const deepsliver = Color(0xFFD2D4E1);
+  // static const sliver = Color(0xFFEBEBEB);
+  // static const grapefruit = Color(0xFFEF5350);
 }
